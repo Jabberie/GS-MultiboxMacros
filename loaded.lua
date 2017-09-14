684572e907dfdc2ff1ca91f81e7398fc7f40863f
Jabberie <jabberie@gmail.com>
2017-09-14 07:20:02 +0100

    Update

--- loaded.lua
+++ loaded.lua
@@ -11,17 +11,22 @@ local GSEPlugin = LibStub("AceAddon-3.0"):NewAddon(ModName, "AceEvent-3.0")
 local function processAddonLoaded(event, arg)
   if arg == ModName then
     GSE.ImportMacroCollection(Sequences)
-    GSE.Print("HP Macros loaded.  This set currently contains macros for ", ModName)
-    GSE.Print("  - Enhancement Shaman ", ModName)
-    GSE.Print("  - Fury Warriors", ModName)
-    GSE.Print("  - Beast Master Hunters", ModName)
-    GSE.Print("  - Unholy , Blood and Frost Death Knights", ModName)
-    GSE.Print("  - Affliction and Demonology Warlock", ModName)
-    GSE.Print("  - Shadow Priest", ModName)
-    GSE.Print("You can find help and also nominate other macros for this set at https://www.wowlazymacros.com", ModName)
-    if GSE.GetCurrentSpecID () == 253 then
-      GSE.Print("Note: The Beast Master hunter macros have been updated.  A number of macros have been consolidated into the HP_TBMain and HP_TBAOE macros.", ModName)
-    end
+    GSE.Print("Multibox Macros loaded.  This set currently contains macros for ", ModName)
+    GSE.Print("DeathKnight : Blood", ModName)
+    GSE.Print("DemonHunter : All Specs", ModName)
+    GSE.Print("Druid : All Specs", ModName)
+    GSE.Print("Hunter : BM", ModName)
+    GSE.Print("Mage : Arcane", ModName)
+    GSE.Print("Monk : Tank/DPS", ModName)
+    GSE.Print("Paladin : All Specs", ModName)
+    GSE.Print("Priest : Disc", ModName)
+    GSE.Print("Rogue : Outlaw", ModName)
+    GSE.Print("Shaman : All Specs", ModName)
+    GSE.Print("Warlock : All Specs", ModName)
+    GSE.Print("Warrior : Prot", ModName)
+--   if GSE.GetCurrentSpecID () == 253 then
+--     GSE.Print("Note: The Beast Master hunter macros have been updated.  A number of macros have been consolidated into the HP_TBMain and HP_TBAOE macros.", ModName)
+--   end
   end
 end
 
