120be50e18a650a0f86b37cb00d9084aa56a8a8e
Jabberie <jabberie@gmail.com>
2018-04-01 02:44:22 +0100

    Update

--- GSEMacros/Hunter/BeastMastery.lua
+++ GSEMacros/Hunter/BeastMastery.lua
@@ -3,11 +3,15 @@ local _, Sequences = ...
 ----- Hunter 
 ------------------
 Sequences['JMM_Hu_BM_ST'] = {
-  Author = "Jabberie@Draka",  
+  Author="Jabberie@draka",
   SpecID=253,
   Talents = "3,1,1,2,3,3,3",
   Default=1,
-  Icon="INV_MISC_QUESTIONMARK",
+  Raid=2,
+  Dungeon=2,
+  Heroic=2,
+  Mythic=2,
+  Icon='ability_hunter_bestialdiscipline',
   MacroVersions = {
     [1] = {
       StepFunction = "Sequential",
@@ -17,6 +21,7 @@ Sequences['JMM_Hu_BM_ST'] = {
         "/petautocaston [nogroup] growl",
       },
       PreMacro={
+        "/castsequence !Volley, null",
       },
         "/cast [nopet][@pet,dead] Revive Pet",
         "/cast Dire Beast",
@@ -24,7 +29,69 @@ Sequences['JMM_Hu_BM_ST'] = {
         "/castsequence Kill Command, Cobra Shot, Cobra Shot, Multi-Shot",
         "/cast [talent:6/1] A Murder of Crows; [talent:6/2] Barrage",
         "/castsequence [combat] Dire Beast, Titan's Thunder",
-        "/castsequence Kill Command, Cobra Shot, Cobra Shot, Multi-Shot",        
+        "/castsequence Kill Command, Cobra Shot, Cobra Shot, Multi-Shot",
+      PostMacro={
+      },
+      KeyRelease={
+        "/cast [combat] Bestial Wrath",
+        "/cast [combat] Blood Fury",
+        "/cast [combat] Berserking",
+        "/cast [combat] Aspect of the Wild",
+      },
+    },
+    [2] = {
+      StepFunction = "Sequential",
+      KeyPress={
+        "/cast [@focus, exists, nodead][@party1, exists, nodead][@pet, exists, nodead] Misdirection",
+        "/petautocastoff [group] Growl",
+        "/petautocaston [nogroup] growl",
+      },
+      PreMacro={
+        "/cancelaura volley",
+      },
+        "/cast [nopet][@pet,dead] Revive Pet",
+        "/cast Dire Beast",
+        "/cast [nopet] Call Pet 1",
+        "/castsequence Kill Command, Cobra Shot, Cobra Shot, Cobra Shot",
+        "/cast [talent:6/1] A Murder of Crows; [talent:6/2] Barrage",
+        "/castsequence [combat] Dire Beast, Titan's Thunder",
+        "/castsequence Kill Command, Cobra Shot, Cobra Shot, Cobra Shot",
+      PostMacro={
+      },
+      KeyRelease={
+        "/cast [combat] Bestial Wrath",
+        "/cast [combat] Blood Fury",
+        "/cast [combat] Berserking",
+        "/cast [combat] Aspect of the Wild",
+      },
+    },
+  },
+}
+
+Sequences['JMM_Hu_BM_AoE'] = {
+  Author="Jabberie@Draka",
+  SpecID=253,
+  Talents = "3,1,1,2,3,3,3",
+  Default=1,
+  Icon='INV_MISC_QUESTIONMARK',
+  MacroVersions = {
+    [1] = {
+      StepFunction = "Sequential",
+      KeyPress={
+        "/cast [@focus, exists, nodead][@party1, exists, nodead][@pet, exists, nodead] Misdirection",
+        "/petautocastoff [group] Growl",
+        "/petautocaston [nogroup] growl",
+      },
+      PreMacro={
+        "/castsequence !Volley, null",
+      },
+        "/cast [nopet][@pet,dead] Revive Pet",
+        "/cast Dire Beast",
+        "/cast [nopet] Call Pet 1",
+        "/castsequence Kill Command, Multi-Shot, Multi-Shot, Multi-Shot",
+        "/cast [talent:6/2] Barrage",
+        "/castsequence [combat] Dire Beast, Titan's Thunder",
+        "/castsequence Kill Command, Multi-Shot, Multi-Shot, Multi-Shot",
       PostMacro={
       },
       KeyRelease={
@@ -36,39 +103,6 @@ Sequences['JMM_Hu_BM_ST'] = {
   },
 }
 
-Sequences['JMM_Hu_BM_AoE'] = {
-  Author = "Jabberie@Draka",  
-  SpecID=253,
-  Talents = "3,1,1,2,3,3,3",
-  Default=1,
-  Icon="INV_MISC_QUESTIONMARK",
-  MacroVersions = {
-    [1] = {
-      StepFunction = "Sequential",
-      KeyPress={
-        "/cast [@focus, exists, nodead][@party1, exists, nodead][@pet, exists, nodead] Misdirection",
-        "/petautocastoff [group] Growl",
-        "/petautocaston [nogroup] growl",
-      },
-      PreMacro={
-      },
-        "/cast [nopet][@pet,dead] Revive Pet",
-        "/cast Dire Beast",
-        "/cast [nopet] Call Pet 1",
-        "/castsequence Kill Command, Multi-Shot, Multi-Shot, Multi-Shot",
-        "/cast [talent:6/1] A Murder of Crows; [talent:6/2] Barrage",
-        "/castsequence [combat] Dire Beast, Titan's Thunder",
-        "/castsequence Kill Command, Multi-Shot, Multi-Shot, Multi-Shot",
-      PostMacro={
-      },
-      KeyRelease={
-        "/cast [combat] Bestial Wrath",
-        "/cast [combat] Blood Fury",
-        "/cast [combat] Aspect of the Wild",
-      },
-    },
-  },
-}
 
 Sequences['JMM_Hu_BM_Farm'] = {
   Author="Jabberie@Draka",
@@ -100,6 +134,7 @@ Sequences['JMM_Hu_BM_Farm'] = {
       KeyRelease={
         "/cast [combat] Bestial Wrath",
         "/cast [combat] Blood Fury",
+        "/cast [combat] Berserking",
         "/cast [combat] Aspect of the Wild",
       },
     },
